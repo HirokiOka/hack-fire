b8aa136ebdb6d4d83f272c6aeb02ef80ca3e1d43
hiroka <mementoka@gmail.com>
2023-11-01 12:28:00 +0900

    add launch.js

---
+++ launch.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+const { app, BrowserWindow } = require('electron');
+
+const deployUrl = "https://battlehacker.adaptable.app";
+const localUrl = "http://localhost:3000";
+
+app.whenRead().then(() => {
+  const { screen } = require('electron');
+  const displays = screen.getAllDisplays();
+  let inputIdx = 1;
+  displays.forEach((d, _) => {
+    const windowWidth = d.workAreaSize.width;
+    const windowHeight = d.workAreaSize.height;
+    console.log(d.label);
+
+    if (d.label === 'T101D') {
+      const x = d.bounds.x;
+      const y = d.bounds.y;
+      const inputWindow = new BrowserWindow({
+        x, y,
+        windowWidth, windowHeight,
+        autoHideMenuBar: true
+      });
+      inputWindow.loadURL(`${deployUrl}/p${inputIdx}_title`);
+      inputWindow.setFullScreen(true);
+      inputIdx++;
+    }
+
+    if (d.label === 'PTWLD-22W') {
+      const x = d.bounds.x;
+      const y = d.bounds.y;
+      const gameWindow = new BrowserWindow({
+        x, y,
+        windowWidth, windowHeight,
+        autoHideMenuBar: true
+      });
+      gameWindow.loadURL(`${deployUrl}`);
+      gameWindow.setFullScreen(true);
+    }
+
+  });
+});
