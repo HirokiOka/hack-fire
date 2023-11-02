b0edad8994e9d2b0dba1a9509afbb58649c5d021
hiroka <mementoka@gmail.com>
2023-11-02 10:59:29 +0900

    add caption on title

--- launch.js
+++ launch.js
@@ -7,25 +7,26 @@ const localUrl = "http://localhost:3000";
 app.whenReady().then(() => {
   const { screen } = require('electron');
   const displays = screen.getAllDisplays();
-  let inputIdx = 1;
   displays.forEach((d, _) => {
     const windowWidth = d.workAreaSize.width;
     const windowHeight = d.workAreaSize.height;
     console.log(d.label);
 
+    //Input Interface
     if (d.label === 'T101D') {
       const x = d.bounds.x;
       const y = d.bounds.y;
+      const displayIndex = x < 0 ? 1 : 2;
       const inputWindow = new BrowserWindow({
         x, y,
         windowWidth, windowHeight,
         autoHideMenuBar: true
       });
-      inputWindow.loadURL(`${deployUrl}/p${inputIdx}_title`);
+      inputWindow.loadURL(`${deployUrl}/p${displayIndex}_title`);
       inputWindow.setFullScreen(true);
-      inputIdx++;
     }
 
+    //Game Display
     if (d.label === 'PTFWLD-22W') {
       const x = d.bounds.x;
       const y = d.bounds.y;
