88519540f0776158402c51842a8f739865e9c9ad
hiroka <mementoka@gmail.com>
2024-01-02 12:33:47 +0900

    add startInput.js

---
+++ startInput.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+const { app, BrowserWindow } = require('electron');
+
+const deployUrl = 'https://battlehacker.adaptable.app/p1_title';
+
+app.whenReady().then(() => {
+  const { screen } = require('electron');
+  const display = screen.getPrimaryDisplay();
+  const inputWindow = new BrowserWindow({
+    display.bounds.x,
+    display.bounds.y, 
+    display.bounds.width, 
+    display.bounds.height, 
+    autoHideMenuBar: true, 
+  });
+  inputWindow.loadURL(deployUrl);
+  inputWindow.setFullScreen(true);
+});
