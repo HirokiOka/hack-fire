e2c34b0b5c0a917eaf7d8ba2036844ce84b8bf13
hiroka <mementoka@gmail.com>
2024-04-29 18:51:02 +0900

    add reload attempts

--- startInput.js
+++ startInput.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, screen } = require('electron');
 
 //const deployUrl = 'http://slash-shot.adaptable.app/p1_title';
 const deployUrl = 'https://battlehacker.adaptable.app/p1_title';
+//const deployUrl = 'dummy';
 
 app.whenReady().then(() => {
   const primaryDisplay = screen.getPrimaryDisplay()
@@ -10,14 +11,23 @@ app.whenReady().then(() => {
     width, height,
     autoHideMenuBar: true, 
   });
-  inputWindow.loadURL(deployUrl)
-    .then(() => {
-      inputWindow.setFullScreen(true);
-    })
-    .catch((error) => {
-      console.error('Error loading URL: ', error);
-      inputWindow.loadFile('error.html');
+
+  function loadUrlWithRetry(url, attempts = 0) {
+    inputWindow.loadURL(deployUrl)
+      .then(() => {
+        inputWindow.setFullScreen(true);
+      })
+      .catch((err) => {
+        console.error('Error loading URL: ', err);
+        if (attempts < 3) {
+          setTimeout(() => loadUrlWithRetry(url, attempts + 1), 5000);
+        } else {
+          inputWindow.loadFile('error.html');
+        }
     });
+  }
+
+  loadUrlWithRetry(deployUrl);
 
   inputWindow.on('closed', () => {
     app.quit();
@@ -27,3 +37,5 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   app.quit();
 });
+
+
